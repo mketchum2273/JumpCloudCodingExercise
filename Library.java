0bc5403905a9be8952fae2b8ce092b5e9d00b042
mketchum2273 <61638735+mketchum2273@users.noreply.github.com>
2020-03-01 19:12:08 -0700

    Add files via upload
    Changed the way I'm handling synchronization in the Library class

--- Library.java
+++ Library.java
@@ -15,47 +15,51 @@ public class Library {
     }
 
     // Update the actions map with the contents of the JSON serialized string being passed in
-    public synchronized void addAction(String actionString) {
-        gson = new Gson();
-        Action newAction = gson.fromJson(actionString, Action.class);
-        String actionName = newAction.getName();
-        int actionTime = newAction.getTime();
+    public void addAction(String actionString) {
+        synchronized(this) {
+            gson = new Gson();
+            Action newAction = gson.fromJson(actionString, Action.class);
+            String actionName = newAction.getName();
+            int actionTime = newAction.getTime();
 
-        if (actions.containsKey(actionName)) {
-            // The actionName already exists as a key, so update the ActionDetails value for the pair
-            ActionDetails previousActionDetails = actions.get(actionName);
-            int updatedTime = previousActionDetails.getTime() + actionTime;
-            int updatedCount = previousActionDetails.getCount() + 1;
-            ActionDetails updatedActionDetails = new ActionDetails(updatedTime, updatedCount);
-            actions.put(actionName, updatedActionDetails);
-        } else {
-            // Add a new action name/details pair to the actions map
-            ActionDetails newActionDetails = new ActionDetails(actionTime, 1);
-            actions.put(actionName, newActionDetails);
+            if (actions.containsKey(actionName)) {
+                // The actionName already exists as a key, so update the ActionDetails value for the pair
+                ActionDetails previousActionDetails = actions.get(actionName);
+                int updatedTime = previousActionDetails.getTime() + actionTime;
+                int updatedCount = previousActionDetails.getCount() + 1;
+                ActionDetails updatedActionDetails = new ActionDetails(updatedTime, updatedCount);
+                actions.put(actionName, updatedActionDetails);
+            } else {
+                // Add a new action name/details pair to the actions map
+                ActionDetails newActionDetails = new ActionDetails(actionTime, 1);
+                actions.put(actionName, newActionDetails);
+            }
         }
     }
 
     // Return the contents of the actions map as an array of JSON serialized strings
-    public synchronized String[] getStats() {
-        String[] actionStats = new String[actions.size()];
-        int index = 0;
-        Iterator it = actions.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            String name = (String) pair.getKey();
-            ActionDetails actionDetails = (ActionDetails) pair.getValue();
-            int detailsTime = actionDetails.getTime();
-            int detailsCount = actionDetails.getCount();
-            int time = (detailsTime + detailsCount - 1) / detailsCount;
-            Action action = new Action(name, time);
+    public String[] getStats() {
+        synchronized(this) {
+            String[] actionStats = new String[actions.size()];
+            int index = 0;
+            Iterator it = actions.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                String name = (String) pair.getKey();
+                ActionDetails actionDetails = (ActionDetails) pair.getValue();
+                int detailsTime = actionDetails.getTime();
+                int detailsCount = actionDetails.getCount();
+                int time = (detailsTime + detailsCount - 1) / detailsCount;
+                Action action = new Action(name, time);
 
-            gson = new Gson();
-            String actionString = gson.toJson(action);
-            actionStats[index] = actionString;
-            index++;
+                gson = new Gson();
+                String actionString = gson.toJson(action);
+                actionStats[index] = actionString;
+                index++;
+            }
+
+            return actionStats;
         }
-
-        return actionStats;
     }
 
     // This getter exists solely for use in a unit test
